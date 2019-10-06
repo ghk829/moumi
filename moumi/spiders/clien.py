e0cace84260de72ab5a682eb70c41c6234add6db
ghk829 <ghk829@hanmail.net>
2019-10-07 01:25:57 +0900

    Revert "사이트마다 구분하고, uuid 생성"
    This reverts commit 96e585cd8a6f96c7d584fba74d1a50442e8e30df.
    
    # Conflicts:
    #	moumi/spiders/clien.py

--- moumi/spiders/clien.py
+++ moumi/spiders/clien.py
@@ -4,7 +4,6 @@ import datetime
 import dateutil
 from dateutil import parser
 import uuid
-from selenium import webdriver
 
 class ClienSpider(scrapy.Spider):
     name = "clien"
@@ -62,7 +61,7 @@ class ClienSpider(scrapy.Spider):
 
             if now - delta > date_time:
                 break
-            result['id'] = "clien_"+str(uuid.uuid4())
+            result['id'] = "clien"+str(uuid.uuid4())
             result['date'] = date
 
             subject = item.css(css_components.get("subject")).pop()
@@ -121,64 +120,55 @@ class ClienSpider(scrapy.Spider):
             yield result
 
     def parse_ppom(self,response,css_components,now,delta):
-        driver = webdriver.PhantomJS("phantomjs")
-        driver.get(response.url)
-        driver.explca(3)
-        from scrapy import Selector
-        selector = Selector(text=driver.page_source)
-
         items = []
         for css in css_components.get("items"):
-            items += selector.css(css)
+            items += response.css(css)
 
         for item in items:
-            try:
-                result = {}
-                result['id'] = "ppom_" + str(uuid.uuid4())
-                for idx,information in enumerate(item.css("td.list_vspace")):
+            result = {}
+            result['id'] = "ppom" + str(uuid.uuid4())
+            for idx,information in enumerate(item.css("td.list_vspace")):
 
-                    if idx ==0:
-                        "아이템 번호"
-                    elif idx ==1:
-                        "기타"
-                    elif idx ==2:
-                        "저자"
-                    elif idx ==3:
-                        "타이틀"
-                        "URL"
-                        href = response.urljoin(information.css("a")[0].attrib['href'])
-                        result['url'] = href
-                        comment = ''.join([e.strip() for e in information.css(".list_comment2")[0].css("::text").extract()])
-                        comment = int(comment)
-                        result['comment'] = comment
-                        if information.css(".list_title"):
-                            result['title'] = ''.join([e.strip() for e in information.css("::text").extract()])
-                        else:
-                            continue
-                    elif idx ==4:
-                        "날짜"
-                        date_str = information.css("::text").get().strip()
-                        date_time = parser.parse(date_str)
-                        date = date_time.strftime("%Y-%m-%d")
-                        if now - delta > date_time:
-                            break
-                        result['date'] = date
+                if idx ==0:
+                    "아이템 번호"
+                elif idx ==1:
+                    "기타"
+                elif idx ==2:
+                    "저자"
+                elif idx ==3:
+                    "타이틀"
+                    "URL"
+                    href = response.urljoin(information.css("a")[0].attrib['href'])
+                    result['url'] = href
+                    comment = ''.join([e.strip() for e in information.css(".list_comment2")[0].css("::text").extract()])
+                    comment = int(comment)
+                    result['comment'] = comment
+                    if information.css(".list_title"):
+                        result['title'] = ''.join([e.strip() for e in information.css("::text").extract()])
+                    else:
+                        continue
+                elif idx ==4:
+                    "날짜"
+                    date_str = information.css("::text").get().strip()
+                    date_time = parser.parse(date_str)
+                    date = date_time.strftime("%Y-%m-%d")
+                    if now - delta > date_time:
+                        break
+                    result['date'] = date
 
-                    elif idx ==5:
-                        "like"
-                        if information.css("::text"):
-                            like= information.css("::text").get().split("-")[0]
-                            like = int(like)
-                        else:
-                            like = 0
-                        result['like'] = like
-                    elif idx ==6:
-                        view = information.css("::text").get().strip()
-                        view = int(view)
-                        result['view'] = view
+                elif idx ==5:
+                    "like"
+                    if information.css("::text"):
+                        like= information.css("::text").get().split("-")[0]
+                        like = int(like)
+                    else:
+                        like = 0
+                    result['like'] = like
+                elif idx ==6:
+                    view = information.css("::text").get().strip()
+                    view = int(view)
+                    result['view'] = view
 
-                result['score'] = view * like + comment * 100
+            result['score'] = view * like + comment * 100
 
-                yield result
-            except Exception as e:
-                pass
+            yield result
